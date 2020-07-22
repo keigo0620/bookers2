60e9e00cd566b4099afea8ed49dee6c373976f1c
vagrant <vagrant@localhost.localdomain>
2020-07-22 11:24:49 +0000

    いいね修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/about', to: 'homes#about'
 
   resources :books , only: [:create, :index, :show, :edit, :index, :update , :destroy] do
-  resources :favorites, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
 end
   resources :users , only: [:show, :edit, :update, :edit, :index]
